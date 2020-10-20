cd238d3355f27ad3b7e8de77ba9e213718240186
jikkajikka <69613539+jikkajikka888@users.noreply.github.com>
2020-10-20 12:14:19 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,7 +27,7 @@ class PrototypesController < ApplicationController
   def edit
     @prototype = Prototype.find(params[:id])
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to root_path
     end
   end
 
